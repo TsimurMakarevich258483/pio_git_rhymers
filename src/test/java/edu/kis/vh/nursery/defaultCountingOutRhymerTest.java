7468caeea27ddff8e5b1b6afd2244f80859ee80c
258483 <258483@edu.p.lodz.pl>
2026-03-25 09:16:52 +0100

    16 zostali stworzone puste testy

---
+++ src/test/java/edu/kis/vh/nursery/defaultCountingOutRhymerTest.java
@@ -0,0 +1,24 @@
+package edu.kis.vh.nursery;
+
+import junit.framework.TestCase;
+
+public class defaultCountingOutRhymerTest extends TestCase {
+
+    public void testGetTotal() {
+    }
+
+    public void testCountIn() {
+    }
+
+    public void testCallCheck() {
+    }
+
+    public void testIsFull() {
+    }
+
+    public void testPeekaboo() {
+    }
+
+    public void testCountOut() {
+    }
+}
