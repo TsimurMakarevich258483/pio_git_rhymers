2cb77e7f6e3d4403da2a6c79115b18218a82450a
258483 <258483@edu.p.lodz.pl>
2026-03-25 09:09:24 +0100

    15 Naprawilem poprzednie bledy

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -5,7 +5,7 @@ import edu.kis.vh.nursery.FIFORhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
 
-class DefaultRhymersFactory implements Rhymersfactory {
+public class DefaultRhymersFactory implements Rhymersfactory {
 
     @Override
     public defaultCountingOutRhymer GetStandardRhymer() {
